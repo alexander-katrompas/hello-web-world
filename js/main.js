76346dbb67a96f28ffd03100f882359ef1449c7a
amk181 <amk181@txstate.edu>
2020-09-18 15:42:22 -0500

    added explanitory comments

--- js/main.js
+++ js/main.js
@@ -6,21 +6,30 @@
 "use strict";
 
 function getRandomColor(){
-  let letters = '0123456789ABCDEF';
-  let color = '#';
+  let letters = '0123456789ABCDEF';  // all the Hexadecimal single digits
+  let color = '#'; // begin the color string
   for (let i = 0; i < 6; i++) {
+    // get 6 random Hex digits
     color += letters[Math.floor(Math.random() * 16)];
   }
+  // show the color choosen in the console for debugging purposes
+  console.log(color)
+  // send the color string back to the caller
   return color;
 }
 
 function colors(){
+    // bring the title element into this script
     let maintitle = document.getElementById("main-title");
-    console.log("setting colors");
-    maintitle.style.color = getRandomColor();
+    console.log("setting colors"); // for debugging
+    // set the color of maintitle
+    maintitle.style.color = getRandomColor(); 
 }
 
+// this brings the button with the id press-me-btn into this script
 let pressme = document.getElementById("press-me-btn");
+// this assigns an anonymous function to the on click listener
 pressme.onclick = function () {
+    // the code in here is the handler for the onclick event
     colors();
 };
